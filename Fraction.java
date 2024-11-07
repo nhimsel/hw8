90a81e90d206431aaffbdae2de91c903a5213237
nhimsel <>
2024-11-07 11:40:47 -0600

    completed

---
+++ Fraction.java
@@ -0,0 +1,54 @@
+package Java.hw8;
+
+public class Fraction 
+{
+    private int num;
+    private int denom;
+
+    public Fraction(int num, int denom) {
+        this.num = num;
+        this.denom = denom;
+    }
+
+    public int getNum()
+    {
+        return this.num;
+    }
+    
+    public int getDenom()
+    {
+        return this.denom;
+    }
+    
+    /* public static Fraction addFractions(Fraction f1, Fraction f2)
+    {
+        Fraction ans = new Fraction(f1.num*f2.denom+f2.num*f1.denom,f1.denom*f2.denom);
+        int gcd = GCD(ans.num, ans.denom);
+        ans.num/=gcd;
+        ans.denom/=gcd;
+        return ans;
+    } */
+
+    public Fraction add(Fraction f)
+    {
+        Fraction ans = new Fraction(this.num*f.denom+f.num*this.denom, this.denom*f.denom);
+        int gcd = GCD(ans.num, ans.denom);
+        ans.num/=gcd; ans.denom/=gcd;
+        return ans;
+
+    }
+
+    public static int GCD(int a, int b)
+    {
+        if (a==0) return b;
+        else if (b==0) return a;
+        else return GCD(b,a%b);
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.num + "/" + this.denom;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package Java.hw8;
+
+public class Main 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(3, 4);
+        // Fraction ans = Fraction.addFractions(f1,f2);
+        Fraction ans = f1.add(f2);
+        System.out.println(f1 + " + " + f2 + " = " + ans);
+    }
+}
