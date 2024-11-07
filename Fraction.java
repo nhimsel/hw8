7827c6445b6613341997d11a206d3c2149bb9ccb
nhimsel <>
2024-11-07 11:49:22 -0600

    minor cleanup

--- Fraction.java
+++ Fraction.java
@@ -18,15 +18,6 @@ public class Fraction
         return this.denom;
     }
     
-    /* public static Fraction addFractions(Fraction f1, Fraction f2)
-    {
-        Fraction ans = new Fraction(f1.num*f2.denom+f2.num*f1.denom,f1.denom*f2.denom);
-        int gcd = GCD(ans.num, ans.denom);
-        ans.num/=gcd;
-        ans.denom/=gcd;
-        return ans;
-    } */
-
     public Fraction add(Fraction f)
     {
         Fraction ans = new Fraction(this.num*f.denom+f.num*this.denom, this.denom*f.denom);
@@ -35,18 +26,17 @@ public class Fraction
         return ans;
 
     }
-
+    
     public static int GCD(int a, int b)
     {
         if (a==0) return b;
         else if (b==0) return a;
         else return GCD(b,a%b);
     }
-
+    
     @Override
     public String toString()
     {
         return this.num + "/" + this.denom;
     }
-
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ public class Main
     {
         Fraction f1 = new Fraction(1, 2);
         Fraction f2 = new Fraction(3, 4);
-        // Fraction ans = Fraction.addFractions(f1,f2);
         Fraction ans = f1.add(f2);
         System.out.println(f1 + " + " + f2 + " = " + ans);
     }
