8d55eb958e345e7e38ea705522e5b6881a656504
nhimsel <>
2024-11-07 11:58:49 -0600

    more testing

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main
     {
         Fraction f1 = new Fraction(1, 2);
         Fraction f2 = new Fraction(3, 4);
-        Fraction ans = f1.add(f2);
-        System.out.println(f1 + " + " + f2 + " = " + ans);
+        Fraction ans1 = f1.add(f2);
+        System.out.println(f1 + " + " + f2 + " = " + ans1);
+	Fraction ans2 = f2.add(f1);
+	System.out.println(f2 + " + " + f1 + " = " + ans2);
     }
 }
